cc14a7d0cb30e0b7cabfb00587b40f83e5046fdd
DaiQiangReal <daiqianghaha@foxmail.com>
2020-06-29 22:25:30 +0800

    #46. 全排列

---
+++ 数据结构/#46. 全排列.js
@@ -0,0 +1,27 @@
+//https://leetcode-cn.com/problems/permutations/
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function (nums) {
+    let result = [];
+    let temp=[];
+    function dfs() {
+        if(temp.length===nums.length){
+            result.push(JSON.parse(JSON.stringify(temp)));
+            return;
+        }
+        for (let i = 0; i < nums.length; i++) {
+            if(temp.indexOf(nums[i])!==-1)
+                continue;
+            temp.push(nums[i]);
+            dfs();
+            temp.pop();
+        }
+        return;
+    }
+    dfs();
+    return result;
+
+};
+console.log(permute([1, 2, 3]));
