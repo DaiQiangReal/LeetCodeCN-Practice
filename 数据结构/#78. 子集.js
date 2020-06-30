4fb9d48a7516b10b608773d82f6ef551c5487b14
DaiQiangReal <daiqianghaha@foxmail.com>
2020-06-30 15:02:12 +0800

    #78. 子集 添加回溯法

--- 数据结构/#78. 子集.js
+++ 数据结构/#78. 子集.js
@@ -5,17 +5,33 @@
  */
 
 var subsets = function (nums) {
-    let array = new Array();
-    array.push([])
-    for (let i = 0; i < nums.length; i++) {
-        let tempArray = new Array();
-        array.forEach((e) => {
-            let array = JSON.parse(JSON.stringify(e));
-            array.push(nums[i]);
-            tempArray.push(array);
-        });
-        array = [...array, ...tempArray];
+    // let array = new Array();
+    // array.push([])
+    // for (let i = 0; i < nums.length; i++) {
+    //     let tempArray = new Array();
+    //     array.forEach((e) => {
+    //         let array = JSON.parse(JSON.stringify(e));
+    //         array.push(nums[i]);
+    //         tempArray.push(array);
+    //     });
+    //     array = [...array, ...tempArray];
+    // }
+    // return array
+
+    let res=[[]];
+    let chosed=[];
+    function dfs(chose){
+        if(chose>nums.length){
+            return;
+        }
+        for(let i=chose;i<nums.length;i++){
+            chosed.push(nums[i])
+            res.push([...chosed]);
+            dfs(i+1);
+            chosed.pop();
+        }
     }
-    return array
+    dfs(0);
+    return res;
 };
 console.log(subsets([1, 2, 3]));
