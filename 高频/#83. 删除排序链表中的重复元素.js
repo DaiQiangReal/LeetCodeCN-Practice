908723cb8354a1dcead429f432395684ff6ef68c
DaiQiangReal <daiqianghaha@foxmail.com>
2020-07-05 16:20:27 +0800

    #83. 删除排序链表中的重复元素

---
+++ 高频/#83. 删除排序链表中的重复元素.js
@@ -0,0 +1,29 @@
+//https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list/
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var deleteDuplicates = function(head) {
+    if(head===null||head.next===null){
+        return  head;
+    }
+    let originHead=head;
+    let slow=head;
+    let fast=head.next;
+    while(fast!==null){
+        if(fast.val!==slow.val){
+            slow.next=fast;
+            slow=slow.next;
+        }
+        fast=fast.next;
+    }
+    slow.next=null;
+    return originHead;
+};
